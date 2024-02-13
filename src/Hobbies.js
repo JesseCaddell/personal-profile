24c0f464815d8ed56e7e6d3b8a5c7a01bb2d6b82
Midgar <80575182+JesseCaddell@users.noreply.github.com>
2024-02-13 01:22:08 -0800

    bolded some text so you could see it better

--- src/Hobbies.js
+++ src/Hobbies.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 const Hobbies = () => {
     return (
-        <div className="hobbies">
+        <div className="hobbies-container">
+            <b><p>Welcome! My name is Lee Kwan-hee and I was on the hit show Singles Inferno.</p></b>
             <h1>Hobbies</h1>
             <ul>
-                <li>Basketball</li>
-                <li>Being a bad youtuber</li>
-                <li>Confusing women</li>
-                <li>Never knowing what I want</li>
+                <li><b>Basketball</b></li>
+                <li><b>Being a bad youtuber</b></li>
+                <li><b>Confusing women</b></li>
+                <li><b>Never knowing what I want</b></li>
             </ul>
         </div>
     );
--- src/Profile.js
+++ src/Profile.js
@@ -4,7 +4,7 @@ const Profile = () => {
     return (
         <div className="profile">
             <img src="./psychopath_outline_320.png" alt="Profile" className="profile-pic"/>
-            <b><p>Welcome! My name is Lee Kwan-hee and I was on the hit show Singles Inferno.</p></b>
+            
         </div>
         
     );
