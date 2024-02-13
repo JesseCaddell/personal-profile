bcdf6c77f3edf0387397a984310e971edcc57b5d
Midgar <80575182+JesseCaddell@users.noreply.github.com>
2024-02-13 01:16:25 -0800

    changed the background to something more appropriate

--- src/Profile.js
+++ src/Profile.js
@@ -4,8 +4,9 @@ const Profile = () => {
     return (
         <div className="profile">
             <img src="./psychopath_outline_320.png" alt="Profile" className="profile-pic"/>
-            <p>Welcome! My name is Lee Kwan-hee and I was on the hit show Singles Inferno.</p>
+            <b><p>Welcome! My name is Lee Kwan-hee and I was on the hit show Singles Inferno.</p></b>
         </div>
+        
     );
 };
 
